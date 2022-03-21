54f55edeb65ac671c1f15896563fb6ce6011e4e2
Rob Curtis <Robanthonycurtis@gmail.com>
2022-03-21 17:42:09 +0000

    Completed javacript-primitives-and-variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,19 @@
+var fullName = 'Robert Curtis';
+console.log('Name:', fullName);
+console.log(typeof 'Robert Curtis');
+
+var isCool = true;
+console.log('Is He Cool?:', isCool);
+console.log(typeof true);
+
+var totalPets = 1;
+console.log('Number of Pets:', totalPets);
+console.log(typeof 1);
+
+var unknown = null;
+console.log('unknown', unknown);
+console.log(typeof null);
+
+var never;
+console.log('never', never);
+console.log(typeof never);
