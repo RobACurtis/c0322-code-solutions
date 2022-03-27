98792cd9b4ab67218363ee783c9d6357cb5cce73
Rob Curtis <Robanthonycurtis@gmail.com>
2022-03-27 04:37:12 +0000

    updated compact

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -7,8 +7,7 @@
 function compact(array) {
   var arr = [];
   for (var i = 0; i < array.length; i++) {
-    if (array[i] !== false && array[i] !== 0 && array[i] !== null &&
-      array[i] !== undefined && array[i] !== '' && !Number.isNaN(array[i])) {
+    if (array[i]) {
       arr.push(array[i]);
     }
   } return arr;
