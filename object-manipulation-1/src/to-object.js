035f6503e71434fe2fe560845ec1283914aa21a7
Rob Curtis <Robanthonycurtis@gmail.com>
2022-03-27 05:06:32 +0000

    completed object-manipulation-1

--- object-manipulation-1/src/to-object.js
+++ object-manipulation-1/src/to-object.js
@@ -1,11 +1,10 @@
 /* exported toObject */
+// create a function
+// make an empty object to store values into
+// assign the zeroed index to the key and the first index to value
+// return the object from the function
 function toObject(keyValuePair) {
   var obj = {};
-  for (var x in keyValuePair) {
-    obj[keyValuePair[x]] = keyValuePair[x];
-  } return obj;
+  obj[keyValuePair[0]] = keyValuePair[1];
+  return obj;
 }
-
-//   var obj = [{ keyValuePair }];
-//   console.log(obj);
-// }
