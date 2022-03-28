b6bda11c677a6a590972ad4f5f0833d6788eaafa
Rob Curtis <Robanthonycurtis@gmail.com>
2022-03-28 19:36:44 +0000

    completed dom-events

---
+++ dom-events/buttons.js
@@ -0,0 +1,27 @@
+function handleClick(event) {
+  console.log('button clicked');
+  console.log('event', event);
+  console.log('target', event.target);
+}
+
+var $click = document.querySelector('.click-button');
+
+$click.addEventListener('click', handleClick);
+
+function handleMouseOver(event) {
+  console.log('button hovered');
+  console.log('event', event);
+  console.log('target', event.target);
+}
+
+var $hover = document.querySelector('.hover-button');
+$hover.addEventListener('mouseover', handleMouseOver);
+
+function handleDoubleClick(event) {
+  console.log('button double-clicked');
+  console.log('event', event);
+  console.log('target', event.target);
+}
+
+var $doubleClick = document.querySelector('.double-click-button');
+$doubleClick.addEventListener('dblclick', handleDoubleClick);
