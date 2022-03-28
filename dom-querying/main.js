a356fe3ee5f0a9de1a6a28c9cfd1e78faaa12794
Rob Curtis <Robanthonycurtis@gmail.com>
2022-03-28 17:37:45 +0000

    completed dom-querying

---
+++ dom-querying/main.js
@@ -0,0 +1,18 @@
+console.log('Hello World');
+var head = document.querySelector('h1');
+console.log('h1: ', head);
+console.dir(head);
+
+var id = document.querySelector('#explanation');
+console.log('id: ', id);
+console.dir(id);
+
+var cls = document.querySelector('.hint');
+console.log('hint: ', cls);
+console.dir(cls);
+
+var all = document.querySelectorAll('p');
+console.log('all: ', all);
+
+var clsAll = document.querySelectorAll('.example-link');
+console.log('class All: ', clsAll);
