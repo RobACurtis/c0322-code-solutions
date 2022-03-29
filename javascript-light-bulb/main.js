51a24c9b6f33917eafcb80dd14207688f30d5894
Rob Curtis <Robanthonycurtis@gmail.com>
2022-03-29 22:07:16 +0000

    updated with boolean

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,16 +1,17 @@
 var $button = document.querySelector('button');
-var $class = $button.className;
 var $body = document.querySelector('body');
+var isLightOn = false;
 
 function lights() {
-  if ($class === 'dark shape') {
+  if (isLightOn === false) {
+    isLightOn = true;
     $button.className = 'light shape';
     $body.className = 'background-light';
   } else {
+    isLightOn = false;
     $button.className = 'dark shape';
     $body.className = 'background-dark';
   }
-  $class = $button.className;
 }
 
 $button.addEventListener('click', lights);
