ecd3743da50099405ff8c7ac8eeee04dd3d3c177
Rob Curtis <Robanthonycurtis@gmail.com>
2022-03-29 21:55:19 +0000

    updated DOM and logic

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,15 +1,16 @@
 var $button = document.querySelector('button');
+var $class = $button.className;
 var $body = document.querySelector('body');
 
 function lights() {
-  if ($button.className === 'light shape') {
-    $button.className = 'dark shape';
-    $body.className = 'background-dark';
-
-  } else {
+  if ($class === 'dark shape') {
     $button.className = 'light shape';
     $body.className = 'background-light';
+  } else {
+    $button.className = 'dark shape';
+    $body.className = 'background-dark';
   }
+  $class = $button.className;
 }
 
 $button.addEventListener('click', lights);
