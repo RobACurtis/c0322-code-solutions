057fb6b34bd8993abbe9c16af7a74e2ef9b53062
Rob Curtis <Robanthonycurtis@gmail.com>
2022-03-30 04:33:38 +0000

    in progress

--- string-manipulation-2/src/capitalize-word.js
+++ string-manipulation-2/src/capitalize-word.js
@@ -1,15 +1,21 @@
 /* exported capitalizeWord */
 // create a function to capitalize first letter of the word
 // bring all letters to lowercase
-// convert string into array
+// check if the word is javascript, if it is, make it JavaScript
+// assign JavScript to a var and return it from the function
+// otherwise convert string into array
 // at the zeroed index bring to uppercase
-// return to string
+// concatenate the rest of the word starting at the first index
+// assign to the variable
+// return string
 
 function capitalizeWord(word) {
   var wrd = word.toLowerCase();
-  var str = wrd.charAt(0).toUpperCase() + wrd.slice(1);
-  if (str === 'Javascript') {
-    str = 'JavaScript';
+  if (wrd === 'javascript') {
+    var str = 'JavaScript';
+    return str;
+  } else {
+    str = wrd.charAt(0).toUpperCase() + wrd.slice(1);
+    return str;
   }
-  return str;
 }
