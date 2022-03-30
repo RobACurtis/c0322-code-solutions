768d1d269915a0a20afb87e09f0369bde797a805
Rob Curtis <Robanthonycurtis@gmail.com>
2022-03-30 21:36:10 +0000

    in progress

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,319 +1,317 @@
 var $span = document.querySelectorAll('span');
-var y = 0;
-var z = 1;
-var el = $span[y];
-var el2 = $span[z];
+var letterIndex = 0;
+var spanLetter = $span[letterIndex];
 
-function key() {
-  var x = event.key;
-  if (y === 0 && x === 'g') {
-    el.className = 'green';
-    el2.className = 'underline';
-    y++;
-    z++;
-    el = $span[y];
-    el2 = $span[z];
-    return y;
+function key(event) {
+  var key = event.key;
+  if (spanLetter.textContent === key) {
+    $span[letterIndex].className = 'green';
+    letterIndex++;
+    $span[letterIndex].className = 'underline';
+    spanLetter = $span[letterIndex];
+    return spanLetter;
   } else {
-    el.className = 'red underline';
-  }
-  if (y === 1 && x === 'r') {
-    el.className = 'green';
-    el2.className = 'underline';
-    y++;
-    z++;
-    el = $span[y];
-    el2 = $span[z];
-    return;
-  } else {
-    el.className = 'red underline';
-  }
-  if (y === 2 && x === 'u') {
-    el.className = 'green';
-    el2.className = 'underline';
-    y++;
-    z++;
-    el = $span[y];
-    el2 = $span[z];
-    return; g;
-  } else {
-    el.className = 'red underline';
-  }
-  if (y === 3 && x === 'm') {
-    el.className = 'green';
-    el2.className = 'underline';
-    y++;
-    z++;
-    el = $span[y];
-    el2 = $span[z];
-    return;
-  } else {
-    el.className = 'red underline';
-  }
-  if (y === 4 && x === 'p') {
-    el.className = 'green';
-    el2.className = 'underline';
-    y++;
-    z++;
-    el = $span[y];
-    el2 = $span[z];
-    return;
-  } else {
-    el.className = 'red underline';
-  }
-  if (y === 5 && x === 'y') {
-    el.className = 'green';
-    el2.className = 'underline';
-    y++;
-    z++;
-    el = $span[y];
-    el2 = $span[z];
-    return;
-  } else {
-    el.className = 'red underline';
-  }
-  if (y === 6 && x === ' ') {
-    el.className = 'green';
-    el2.className = 'underline';
-    y++;
-    z++;
-    el = $span[y];
-    el2 = $span[z];
-    return;
-  } else {
-    el.className = 'red underline';
-  }
-  if (y === 7 && x === 'w') {
-    el.className = 'green';
-    el2.className = 'underline';
-    y++;
-    z++;
-    el = $span[y];
-    el2 = $span[z];
-    return;
-  } else {
-    el.className = 'red underline';
-  }
-  if (y === 8 && x === 'i') {
-    el.className = 'green';
-    el2.className = 'underline';
-    y++;
-    z++;
-    el = $span[y];
-    el2 = $span[z];
-    return;
-  } else {
-    el.className = 'red underline';
-  }
-  if (y === 9 && x === 'z') {
-    el.className = 'green';
-    el2.className = 'underline';
-    y++;
-    z++;
-    el = $span[y];
-    el2 = $span[z];
-    return;
-  } else {
-    el.className = 'red underline';
-  }
-  if (y === 10 && x === 'a') {
-    el.className = 'green';
-    el2.className = 'underline';
-    y++;
-    z++;
-    el = $span[y];
-    el2 = $span[z];
-    return;
-  } else {
-    el.className = 'red underline';
-  }
-  if (y === 11 && x === 'r') {
-    el.className = 'green';
-    el2.className = 'underline';
-    y++;
-    z++;
-    el = $span[y];
-    el2 = $span[z];
-    return;
-  } else {
-    el.className = 'red underline';
-  }
-  if (y === 12 && x === 'd') {
-    el.className = 'green';
-    el2.className = 'underline';
-    y++;
-    z++;
-    el = $span[y];
-    el2 = $span[z];
-    return;
-  } else {
-    el.className = 'red underline';
-  }
-  if (y === 13 && x === 's') {
-    el.className = 'green';
-    el2.className = 'underline';
-    y++;
-    z++;
-    el = $span[y];
-    el2 = $span[z];
-    return;
-  } else {
-    el.className = 'red underline';
-  }
-  if (y === 14 && x === ' ') {
-    el.className = 'green';
-    el2.className = 'underline';
-    y++;
-    z++;
-    el = $span[y];
-    el2 = $span[z];
-    return;
-  } else {
-    el.className = 'red underline';
-  } if (y === 15 && x === 'm') {
-    el.className = 'green';
-    el2.className = 'underline';
-    y++;
-    z++;
-    el = $span[y];
-    el2 = $span[z];
-    return;
-  } else {
-    el.className = 'red underline';
-  } if (y === 16 && x === 'a') {
-    el.className = 'green';
-    el2.className = 'underline';
-    y++;
-    z++;
-    el = $span[y];
-    el2 = $span[z];
-    return;
-  } else {
-    el.className = 'red underline';
-  } if (y === 17 && x === 'k') {
-    el.className = 'green';
-    el2.className = 'underline';
-    y++;
-    z++;
-    el = $span[y];
-    el2 = $span[z];
-    return;
-  } else {
-    el.className = 'red underline';
-  } if (y === 18 && x === 'e') {
-    el.className = 'green';
-    el2.className = 'underline';
-    y++;
-    z++;
-    el = $span[y];
-    el2 = $span[z];
-    return;
-  } else {
-    el.className = 'red underline';
-  } if (y === 19 && x === ' ') {
-    el.className = 'green';
-    el2.className = 'underline';
-    y++;
-    z++;
-    el = $span[y];
-    el2 = $span[z];
-    return;
-  } else {
-    el.className = 'red underline';
-  } if (y === 20 && x === 't') {
-    el.className = 'green';
-    el2.className = 'underline';
-    y++;
-    z++;
-    el = $span[y];
-    el2 = $span[z];
-    return;
-  } else {
-    el.className = 'red underline';
-  } if (y === 21 && x === 'o') {
-    el.className = 'green';
-    el2.className = 'underline';
-    y++;
-    z++;
-    el = $span[y];
-    el2 = $span[z];
-    return;
-  } else {
-    el.className = 'red underline';
-  } if (y === 22 && x === 'x') {
-    el.className = 'green';
-    el2.className = 'underline';
-    y++;
-    z++;
-    el = $span[y];
-    el2 = $span[z];
-    return;
-  } else {
-    el.className = 'red underline';
-  } if (y === 23 && x === 'i') {
-    el.className = 'green';
-    el2.className = 'underline';
-    y++;
-    z++;
-    el = $span[y];
-    el2 = $span[z];
-    return;
-  } else {
-    el.className = 'red underline';
-  } if (y === 24 && x === 'c') {
-    el.className = 'green';
-    el2.className = 'underline';
-    y++;
-    z++;
-    el = $span[y];
-    el2 = $span[z];
-    return;
-  } else {
-    el.className = 'red underline';
-  } if (y === 25 && x === ' ') {
-    el.className = 'green';
-    el2.className = 'underline';
-    y++;
-    z++;
-    el = $span[y];
-    el2 = $span[z];
-    return;
-  } else {
-    el.className = 'red underline';
-  } if (y === 26 && x === 'b') {
-    el.className = 'green';
-    el2.className = 'underline';
-    y++;
-    z++;
-    el = $span[y];
-    el2 = $span[z];
-    return;
-  } else {
-    el.className = 'red underline';
-  } if (y === 27 && x === 'r') {
-    el.className = 'green';
-    el2.className = 'underline';
-    y++;
-    z++;
-    el = $span[y];
-    el2 = $span[z];
-    return;
-  } else {
-    el.className = 'red underline';
-  } if (y === 28 && x === 'e') {
-    el.className = 'green';
-    el2.className = 'underline';
-    y++;
-    z++;
-    el = $span[y];
-    el2 = $span[z];
-    return;
-  } else {
-    el.className = 'red underline';
-  } if (y === 29 && x === 'w') {
-    el.className = 'green';
-  } else {
-    el.className = 'red underline';
+    $span[letterIndex].className = 'red underline';
   }
 }
+//   }
+//   if (y === 1 && x === 'r') {
+//     el.className = 'green';
+//     el2.className = 'underline';
+//     y++;
+//     z++;
+//     el = $span[y];
+//     el2 = $span[z];
+//     return;
+//   } else {
+//     el.className = 'red underline';
+//   }
+//   if (y === 2 && x === 'u') {
+//     el.className = 'green';
+//     el2.className = 'underline';
+//     y++;
+//     z++;
+//     el = $span[y];
+//     el2 = $span[z];
+//     return; g;
+//   } else {
+//     el.className = 'red underline';
+//   }
+//   if (y === 3 && x === 'm') {
+//     el.className = 'green';
+//     el2.className = 'underline';
+//     y++;
+//     z++;
+//     el = $span[y];
+//     el2 = $span[z];
+//     return;
+//   } else {
+//     el.className = 'red underline';
+//   }
+//   if (y === 4 && x === 'p') {
+//     el.className = 'green';
+//     el2.className = 'underline';
+//     y++;
+//     z++;
+//     el = $span[y];
+//     el2 = $span[z];
+//     return;
+//   } else {
+//     el.className = 'red underline';
+//   }
+//   if (y === 5 && x === 'y') {
+//     el.className = 'green';
+//     el2.className = 'underline';
+//     y++;
+//     z++;
+//     el = $span[y];
+//     el2 = $span[z];
+//     return;
+//   } else {
+//     el.className = 'red underline';
+//   }
+//   if (y === 6 && x === ' ') {
+//     el.className = 'green';
+//     el2.className = 'underline';
+//     y++;
+//     z++;
+//     el = $span[y];
+//     el2 = $span[z];
+//     return;
+//   } else {
+//     el.className = 'red underline';
+//   }
+//   if (y === 7 && x === 'w') {
+//     el.className = 'green';
+//     el2.className = 'underline';
+//     y++;
+//     z++;
+//     el = $span[y];
+//     el2 = $span[z];
+//     return;
+//   } else {
+//     el.className = 'red underline';
+//   }
+//   if (y === 8 && x === 'i') {
+//     el.className = 'green';
+//     el2.className = 'underline';
+//     y++;
+//     z++;
+//     el = $span[y];
+//     el2 = $span[z];
+//     return;
+//   } else {
+//     el.className = 'red underline';
+//   }
+//   if (y === 9 && x === 'z') {
+//     el.className = 'green';
+//     el2.className = 'underline';
+//     y++;
+//     z++;
+//     el = $span[y];
+//     el2 = $span[z];
+//     return;
+//   } else {
+//     el.className = 'red underline';
+//   }
+//   if (y === 10 && x === 'a') {
+//     el.className = 'green';
+//     el2.className = 'underline';
+//     y++;
+//     z++;
+//     el = $span[y];
+//     el2 = $span[z];
+//     return;
+//   } else {
+//     el.className = 'red underline';
+//   }
+//   if (y === 11 && x === 'r') {
+//     el.className = 'green';
+//     el2.className = 'underline';
+//     y++;
+//     z++;
+//     el = $span[y];
+//     el2 = $span[z];
+//     return;
+//   } else {
+//     el.className = 'red underline';
+//   }
+//   if (y === 12 && x === 'd') {
+//     el.className = 'green';
+//     el2.className = 'underline';
+//     y++;
+//     z++;
+//     el = $span[y];
+//     el2 = $span[z];
+//     return;
+//   } else {
+//     el.className = 'red underline';
+//   }
+//   if (y === 13 && x === 's') {
+//     el.className = 'green';
+//     el2.className = 'underline';
+//     y++;
+//     z++;
+//     el = $span[y];
+//     el2 = $span[z];
+//     return;
+//   } else {
+//     el.className = 'red underline';
+//   }
+//   if (y === 14 && x === ' ') {
+//     el.className = 'green';
+//     el2.className = 'underline';
+//     y++;
+//     z++;
+//     el = $span[y];
+//     el2 = $span[z];
+//     return;
+//   } else {
+//     el.className = 'red underline';
+//   } if (y === 15 && x === 'm') {
+//     el.className = 'green';
+//     el2.className = 'underline';
+//     y++;
+//     z++;
+//     el = $span[y];
+//     el2 = $span[z];
+//     return;
+//   } else {
+//     el.className = 'red underline';
+//   } if (y === 16 && x === 'a') {
+//     el.className = 'green';
+//     el2.className = 'underline';
+//     y++;
+//     z++;
+//     el = $span[y];
+//     el2 = $span[z];
+//     return;
+//   } else {
+//     el.className = 'red underline';
+//   } if (y === 17 && x === 'k') {
+//     el.className = 'green';
+//     el2.className = 'underline';
+//     y++;
+//     z++;
+//     el = $span[y];
+//     el2 = $span[z];
+//     return;
+//   } else {
+//     el.className = 'red underline';
+//   } if (y === 18 && x === 'e') {
+//     el.className = 'green';
+//     el2.className = 'underline';
+//     y++;
+//     z++;
+//     el = $span[y];
+//     el2 = $span[z];
+//     return;
+//   } else {
+//     el.className = 'red underline';
+//   } if (y === 19 && x === ' ') {
+//     el.className = 'green';
+//     el2.className = 'underline';
+//     y++;
+//     z++;
+//     el = $span[y];
+//     el2 = $span[z];
+//     return;
+//   } else {
+//     el.className = 'red underline';
+//   } if (y === 20 && x === 't') {
+//     el.className = 'green';
+//     el2.className = 'underline';
+//     y++;
+//     z++;
+//     el = $span[y];
+//     el2 = $span[z];
+//     return;
+//   } else {
+//     el.className = 'red underline';
+//   } if (y === 21 && x === 'o') {
+//     el.className = 'green';
+//     el2.className = 'underline';
+//     y++;
+//     z++;
+//     el = $span[y];
+//     el2 = $span[z];
+//     return;
+//   } else {
+//     el.className = 'red underline';
+//   } if (y === 22 && x === 'x') {
+//     el.className = 'green';
+//     el2.className = 'underline';
+//     y++;
+//     z++;
+//     el = $span[y];
+//     el2 = $span[z];
+//     return;
+//   } else {
+//     el.className = 'red underline';
+//   } if (y === 23 && x === 'i') {
+//     el.className = 'green';
+//     el2.className = 'underline';
+//     y++;
+//     z++;
+//     el = $span[y];
+//     el2 = $span[z];
+//     return;
+//   } else {
+//     el.className = 'red underline';
+//   } if (y === 24 && x === 'c') {
+//     el.className = 'green';
+//     el2.className = 'underline';
+//     y++;
+//     z++;
+//     el = $span[y];
+//     el2 = $span[z];
+//     return;
+//   } else {
+//     el.className = 'red underline';
+//   } if (y === 25 && x === ' ') {
+//     el.className = 'green';
+//     el2.className = 'underline';
+//     y++;
+//     z++;
+//     el = $span[y];
+//     el2 = $span[z];
+//     return;
+//   } else {
+//     el.className = 'red underline';
+//   } if (y === 26 && x === 'b') {
+//     el.className = 'green';
+//     el2.className = 'underline';
+//     y++;
+//     z++;
+//     el = $span[y];
+//     el2 = $span[z];
+//     return;
+//   } else {
+//     el.className = 'red underline';
+//   } if (y === 27 && x === 'r') {
+//     el.className = 'green';
+//     el2.className = 'underline';
+//     y++;
+//     z++;
+//     el = $span[y];
+//     el2 = $span[z];
+//     return;
+//   } else {
+//     el.className = 'red underline';
+//   } if (y === 28 && x === 'e') {
+//     el.className = 'green';
+//     el2.className = 'underline';
+//     y++;
+//     z++;
+//     el = $span[y];
+//     el2 = $span[z];
+//     return;
+//   } else {
+//     el.className = 'red underline';
+//   } if (y === 29 && x === 'w') {
+//     el.className = 'green';
+//   } else {
+//     el.className = 'red underline';
+//   }
+// }
 document.addEventListener('keydown', key);
