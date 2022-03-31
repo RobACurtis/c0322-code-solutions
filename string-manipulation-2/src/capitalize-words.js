d8e80fdabebe46c71857b41a2031486658b54bf7
Rob Curtis <Robanthonycurtis@gmail.com>
2022-03-31 22:49:08 +0000

    in progress

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -1,1 +1,14 @@
 /* exported capitalizeWords */
+function capitalizeWords(string) {
+  var lowercase = string.toLowerCase();
+  var arr = lowercase.split(' ');
+  var str = '';
+  for (var i = 0; i < arr.length; i++) {
+    var letter = arr[i].split('');
+    var upperCase = letter[0].toUpperCase();
+    var wrd = upperCase + arr[i].slice(1) + ' ';
+    str += wrd;
+  }
+  // console.log(str);
+  return str;
+}
