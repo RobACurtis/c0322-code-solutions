b0888f6f9c9ca027dfaa738776708f24ff650576
Rob Curtis <Robanthonycurtis@gmail.com>
2022-04-01 19:47:54 +0000

    completed string-manipulation-2

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,4 +1,13 @@
 /* exported swapChars */
+// create a function to swap characters
+// create a variable for the final string to be stored later
+// split the string by each character
+// get the firstindex of the string and replace it with a character to keep the length the same
+// get the second index of the string and assign both to there own  variable
+// put the secondIndex in the firsts place and delete the useless character
+// put the first index in the seconds place
+// loop through the string array and concatenate and assign the new word to the final string
+// return the final string.
 
 function swapChars(firstIndex, secondIndex, string) {
   var finalStr = '';
