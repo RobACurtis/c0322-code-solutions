6b913e52696b801eca7c4b60d88465c57f4e5a23
Rob Curtis <Robanthonycurtis@gmail.com>
2022-04-01 04:54:46 +0000

    in progress

--- string-manipulation-2/src/first-chars.js
+++ string-manipulation-2/src/first-chars.js
@@ -1,1 +1,18 @@
 /* exported firstChars */
+// create a function to split the string at the length parameter
+// split the string into characters to be counted
+// delete the characters after the length provided.
+// create a variable for the final string to be returned to
+// loop through the array with the shortend characters
+// add them to the returned string variable
+// return the final string from the function
+
+function firstChars(length, string) {
+  var str = string.split('');
+  str.splice(length);
+  var finalString = '';
+  for (var i = 0; i < str.length; i++) {
+    finalString += str[i];
+  }
+  return finalString;
+}
--- string-manipulation-2/src/num-vowels.js
+++ string-manipulation-2/src/num-vowels.js
@@ -1,1 +1,33 @@
 /* exported numVowels */
+// create a function to count the number of vowels
+// bring the string to lowercase
+// split the string into each letter in order to loop
+// create a variable to hold the number of vowels
+// create an array to hold the vowels
+// loop through each string
+// is the string at index equal to any of the first vowel?
+// if it is equal to the first vowel increment the number variable by one.
+// repeat for each index of the numbers variable
+// if none are equal, then do nothing with it
+// at the end of each loop return the numbers variable from the function.
+
+function numVowels(string) {
+  var lowerCase = string.toLowerCase();
+  var str = lowerCase.split('');
+  var num = 0;
+  var vowels = ['a', 'e', 'i', 'o', 'u'];
+
+  for (var i = 0; i < str.length; i++) {
+    if (str[i] === vowels[0]) {
+      num++;
+    } else if (str[i] === vowels[1]) {
+      num++;
+    } else if (str[i] === vowels[2]) {
+      num++;
+    } else if (str[i] === vowels[3]) {
+      num++;
+    } else if (str[i] === vowels[4]) {
+      num++;
+    }
+  } return num;
+}
