36d5458d9c0817d167583295ed3b1e1a51345ea2
Rob Curtis <Robanthonycurtis@gmail.com>
2022-04-01 18:54:16 +0000

    in progress

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,1 +1,14 @@
 /* exported swapChars */
+
+function swapChars(firstIndex, secondIndex, string) {
+  var finalStr = '';
+  var str = string.split('');
+  var firstI = str.splice(firstIndex, 1, ' ');
+  var secondI = str.splice(secondIndex, 1);
+  str.splice(firstIndex, 1, secondI[0]);
+  str.splice(secondIndex, 0, firstI[0]);
+  for (var i = 0; i < str.length; i++) {
+    finalStr += str[i];
+  }
+  return finalStr;
+}
