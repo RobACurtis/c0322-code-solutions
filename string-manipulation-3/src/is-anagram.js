6a46970cfdb3fd606fc32d1662f8cb4ff3a38948
Rob Curtis <Robanthonycurtis@gmail.com>
2022-04-05 15:27:49 +0000

    in progress is-anagram

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,1 +1,22 @@
 /* exported isAnagram */
+function isAnagram(firstString, secondString) {
+  var firstStr = firstString.split('');
+  var secondStr = secondString.split('');
+  for (var secondI = 0; secondI < secondStr.length; secondI++) {
+    for (var firstI = 0; firstI < firstStr.length; firstI++) {
+      if (secondStr[secondI] === firstStr[firstI]) {
+        secondStr[secondI] = true;
+        console.log(secondStr);
+      }
+    }
+  } for (secondI = 0; secondI < secondStr.length; secondI++) {
+    if (secondStr[secondI] !== true) {
+      secondStr = false;
+    } else {
+      secondStr = true;
+    }
+  }
+  console.log(secondStr);
+  return secondStr;
+
+}
