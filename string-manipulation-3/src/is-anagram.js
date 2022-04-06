36642d3ca9cc36b10a3d5737a4917aec2f5f512a
Rob Curtis <Robanthonycurtis@gmail.com>
2022-04-06 00:54:44 +0000

    completed is-anagram

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,4 +1,21 @@
 /* exported isAnagram */
+// create a function to compare two words
+// split the first string by each letter
+// split the second string by each letter
+// loop through the first string
+// check if the index is a space,
+// if its a space, then delete it from the string
+// loop through the second string
+// check if the index is a space, if it is, delete it
+// then check if the first string index is found in the second string
+// if it is delete that character from the second string
+// then break from the loop
+// after the function is done looping
+// if the string is an Anagram, the array will have 0 characters
+// if the second string length is equal to 0 characters
+// return true
+// else return false
+
 function isAnagram(firstString, secondString) {
   var firstStr = firstString.split('');
   var secondStr = secondString.split('');
@@ -11,9 +28,10 @@ function isAnagram(firstString, secondString) {
         secondStr.splice(secondI, 1);
       } else if (secondStr[secondI] === firstStr[firstI]) {
         secondStr.splice(secondI, 1);
+        break;
       }
     }
-  } console.log(secondStr);
+  }
   if (secondStr.length === 0) {
     return true;
   } else {
