8a3861746384d61a0413eff00a57420f229e80d4
Rob Curtis <Robanthonycurtis@gmail.com>
2022-04-06 04:29:47 +0000

    in progress reverse-words

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,1 +1,20 @@
 /* exported reverseWords */
+function reverseWords(string) {
+  var space = string.split(' ');
+  console.log('space', space);
+  var letter = [];
+  var finalStr = '';
+  for (var i = 0; i < space.length; i++) {
+    var str = space[i].split('');
+    letter.push(str);
+  }
+  for (i = 0; i < letter.length; i++) {
+    var rev = letter[i].reverse();
+    console.log('rev', rev);
+    for (var revI = 0; revI < rev.length; revI++) {
+      finalStr += rev.join(rev[revI]);
+      continue;
+    }
+  }
+  console.log(finalStr);
+}
