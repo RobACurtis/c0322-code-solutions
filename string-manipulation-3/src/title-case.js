68d6eadf02a756ae1b23bc7b4980374dd96f0701
Rob Curtis <Robanthonycurtis@gmail.com>
2022-04-07 05:08:57 +0000

    in progress title-case

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,1 +1,29 @@
 /* exported titleCase */
+function titleCase(string) {
+  var lowercase = string.toLowerCase();
+  var splitWord = lowercase.split(' ');
+  var joinedWord = '';
+  var arr = [];
+  for (var i = 0; i < splitWord.length; i++) {
+    var splitLetter = splitWord[i].split('');
+
+    // looks for letter after hyphen and capitilizes it and returns it to astring
+    for (var letterIndex = 0; letterIndex < splitLetter.length; letterIndex++) {
+      if (splitLetter[letterIndex] === '-') {
+        var nextLetter = letterIndex + 1;
+        var caps = splitLetter[nextLetter].toUpperCase();
+        splitLetter[nextLetter] = caps;
+      }
+
+    }
+    joinedWord += splitLetter.join('') + ' ';
+  }
+  arr = joinedWord.split(' ');
+  console.log(arr);
+
+  for (i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+    console.log(arr[i]);
+  }
+}
+// problem.... its one array with only one
