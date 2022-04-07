3d76eb639a947f21030598a299721dd3dd3b03ff
Rob Curtis <Robanthonycurtis@gmail.com>
2022-04-07 03:25:51 +0000

    completed reverseWords

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,20 +1,28 @@
 /* exported reverseWords */
+// create a function to reverse each word in a string
+// split the string at each word
+// create an empty string to store the return string
+// loop through the string for each word
+// for each word split each letter
+// reverse the word
+// join the array of reversed letters
+// check if its the last index,
+// if its the last index, join the reversed word to the final string
+// if its not the last index, join each word to the string and concatenate a space to the end of it
+// return the final string
+
 function reverseWords(string) {
-  var space = string.split(' ');
-  console.log('space', space);
-  var letter = [];
+  var word = string.split(' ');
   var finalStr = '';
-  for (var i = 0; i < space.length; i++) {
-    var str = space[i].split('');
-    letter.push(str);
-  }
-  for (i = 0; i < letter.length; i++) {
-    var rev = letter[i].reverse();
-    console.log('rev', rev);
-    for (var revI = 0; revI < rev.length; revI++) {
-      finalStr += rev.join(rev[revI]);
-      continue;
+  for (var i = 0; i < word.length; i++) {
+    var str = word[i].split('');
+    var reverseStr = str.reverse('');
+    var join = reverseStr.join('');
+    if (i === word.length - 1) {
+      finalStr += join;
+    } else {
+      finalStr += join + ' ';
     }
   }
-  console.log(finalStr);
+  return finalStr;
 }
