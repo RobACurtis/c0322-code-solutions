d37ad6a50d5d749cd5c379d71ef180086a920d77
Rob Curtis <Robanthonycurtis@gmail.com>
2022-04-09 03:13:12 +0000

    completed flatten

--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -1,1 +1,10 @@
 /* exported flatten */
+// create a function to combine arrays with their sub arrays
+// create a variable to take in the array
+// create a new array and concatenate it with the values inside of the original array
+// return the new array from the function.
+
+function flatten(array) {
+  var flatten = [].concat.apply([], array);
+  return flatten;
+}
