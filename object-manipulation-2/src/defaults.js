579fdc97411eafa3105b1cd2ac958287dcaf5414
Rob Curtis <Robanthonycurtis@gmail.com>
2022-04-11 20:12:39 +0000

    in progress default

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -3,7 +3,20 @@
 // assign the values of target to source to make them equal
 // then assign all the properties in source back to target
 
-function defaults(target, source) {
-  Object.assign(source, target);
-  Object.assign(target, source);
-}
+// function defaults(target, source) {
+//   Object.assign(source, target);
+//   Object.assign(target, source);
+// }
+// function defaults(target, source) {
+//   for (var x in source) {
+//     if (target === {}) {
+//       target = source;
+//     }
+//     for (var y in target) {
+//       if (x !== y) {
+//         target[source[y]];
+//       }
+//     }
+//   }
+//   console.log(target);
+// }
