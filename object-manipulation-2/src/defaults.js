0fdada53e317e9a9854469d0ee02d8a408baafc4
Rob Curtis <Robanthonycurtis@gmail.com>
2022-04-11 22:46:13 +0000

    updated object-manipulation-2

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,23 +1,24 @@
 /* exported defaults */
 // create a function to move the properties of the source object into the target object that aren't already in the target object
-// assign the values of target to source to make them equal
-// then assign all the properties in source back to target
+// loop through the source object
+// loop through the target object
+// if the key matches to the key in the target
+// delete the key and continue the function
+// loop through the updated keys object
+// assign each value to the target object
+// return the target object from the function.
 
-// function defaults(target, source) {
-//   Object.assign(source, target);
-//   Object.assign(target, source);
-// }
-// function defaults(target, source) {
-//   var obj = target;
-//   for (var x in source) {
-//     for (var y in target) {
-//       if (target[y] === undefined) {
-//         obj = source[x];
-//       }
-//       if (source[x] !== target[y]) {
-//         obj = source[y];
-//       }
-//     }
-//     console.log(obj);
-//   }
-// }
+function defaults(target, source) {
+  for (var key in source) {
+    for (var keyT in target) {
+      if (key === keyT) {
+        delete source[key];
+        continue;
+      }
+    }
+  }
+  for (key in source) {
+    target[key] = source[key];
+  }
+  return target;
+}
