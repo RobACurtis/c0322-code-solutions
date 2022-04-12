7d25a851089bc6b86637d27c1b52cc558c99c589
Rob Curtis <Robanthonycurtis@gmail.com>
2022-04-12 00:54:42 +0000

    updated pseudocode

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,10 +1,11 @@
 /* exported defaults */
 // create a function to move the properties of the source object into the target object that aren't already in the target object
+// create an empty object literal for matchng keys
 // loop through the source object
 // loop through the target object
 // if the key matches to the key in the target
-// delete the key and continue the function
-// loop through the updated keys object
+// assign it to the object foundKeys.
+// loop through the foundKeys object
 // assign each value to the target object
 // return the target object from the function.
 
