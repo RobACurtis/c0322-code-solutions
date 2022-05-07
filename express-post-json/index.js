954e58a49fac155105d4c4eefb82ac77ba51b86a
Rob Curtis <Robanthonycurtis@gmail.com>
2022-05-07 04:07:06 +0000

    completed express-post-json

--- express-post-json/index.js
+++ express-post-json/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 let nextId = 1;
 const grades = {};
-let gradesArray = [];
+const gradesArray = [];
 
 app.get('/api/grades', (req, res) => {
   res.json(gradesArray);
@@ -11,16 +11,14 @@ app.get('/api/grades', (req, res) => {
 app.use(express.json());
 
 app.post('/api/grades', (req, res) => {
-  res.status(201).send({req.body[0].id: nextId,});
   grades[nextId] = req.body;
-  gradesArray = [];
-  for (const key in grades) {
-    gradesArray.push(grades[key]);
-  }
-  console.log(gradesArray);
+  grades[nextId].id = nextId;
+  gradesArray.push(grades[nextId]);
+  res.status(201).send(grades);
   nextId++;
 });
 
 app.listen(3000, () => {
+  // eslint-disable-next-line no-console
   console.log('server listening');
 });
