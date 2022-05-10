21c276cd44a336b9a821e33b6c6c904eb2cfe070
Rob Curtis <Robanthonycurtis@gmail.com>
2022-05-10 00:00:56 +0000

    completed express-notes-api

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -43,6 +43,46 @@ app.post('/api/notes', (req, res) => {
   }
 });
 
+app.delete('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.status(400).send({ error: 'id must be a positive integer' });
+  } else if (!data.notes[id]) {
+    res.status(404).send({ error: `can not find note with id ${id}` });
+  } else {
+    delete data.notes[id];
+    const stringData = JSON.stringify(data, null, 2);
+    fs.writeFile('./data.json', stringData, 'utf8', err => {
+      if (err) {
+        console.error(err);
+        res.status(500).send({ error: 'an unexpected error occured.' });
+      } else {
+        res.sendStatus(204);
+      }
+    });
+  }
+});
+
+app.put('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.status(400).send({ error: 'id must be a positive integer' });
+  } else if (!req.body.content) {
+    res.status(400).send({ error: 'content is a required field' });
+  } else {
+    data.notes[id] = req.body;
+    const stringData = JSON.stringify(data, null, 2);
+    fs.writeFile('./data.json', stringData, 'utf8', err => {
+      if (err) {
+        console.error(err);
+        res.status(500).send({ error: 'an unexpected error occured.' });
+      } else {
+        res.status(200).send(data.notes[id]);
+      }
+    });
+  }
+});
+
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
   console.log('server listening on port 3000');
