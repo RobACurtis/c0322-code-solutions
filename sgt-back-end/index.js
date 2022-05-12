dcf2f4fc65524bdaabe76d5b91ab11ec9cc4c4b9
Rob Curtis <Robanthonycurtis@gmail.com>
2022-05-12 22:59:45 +0000

    completed post request functionality

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -19,13 +19,42 @@ app.get('/api/grades', (req, res) => {
       if (!grade) {
         res.status(404).json({ error: 'Cannot find grades' });
       } else {
-        res.json(grade);
+        res.status(200).json(grade);
       }
     })
     .catch(err => {
       console.error(err);
       res.status(500).json({ error: 'an unexpected error occured.' });
     });
+});
+
+app.use(express.json());
+
+app.post('/api/grades', (req, res) => {
+  if (!req.body.name || !req.body.course || !req.body.score) {
+    res.status(400).send({ error: 'fill out all fields' });
+  } else {
+    const sql = `
+  insert into "grades" ("name", "course", "score")
+  values ($1, $2, $3)
+  returning *;
+  `;
+
+    const params = [req.body.name, req.body.course, req.body.score];
+    db.query(sql, params)
+      .then(result => {
+        const grades = result.rows;
+        if (!grades) {
+          res.status(404).json({ error: 'data table not found' });
+        } else {
+          res.status(201).json(grades);
+        }
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).json({ error: 'an unexpected error occured.' });
+      });
+  }
 });
 
 app.listen(3000, () => {
