9caa0435678d6e5cc58bd7eaf2801ea8215e7c92
Rob Curtis <Robanthonycurtis@gmail.com>
2022-05-19 11:52:44 +0000

    finished reset and toggle methods

--- react-stopwatch-component/src/stopwatch.jsx
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -4,6 +4,7 @@ export default class StopWatch extends React.Component {
   constructor(props) {
     super(props);
     this.toggle = this.toggle.bind(this);
+    this.reset = this.reset.bind(this);
     this.state = {
       isOn: false,
       time: 0
@@ -14,22 +15,33 @@ export default class StopWatch extends React.Component {
     this.setState({
       isOn: !this.state.isOn
     });
+    this.tick();
   }
 
-  componentDidMount() {
-    this.timerID = setInterval(() => this.tick(), 1000);
+  reset() {
+    if (!this.state.isOn) {
+      this.setState({
+        time: 0
+      });
+    }
   }
 
   tick() {
-    let time = this.state.time;
-    this.setState({ time: time++ });
+    if (!this.state.isOn) {
+      this.timerID = setInterval(() => {
+        const time = this.state.time;
+        this.setState({ time: time + 1 });
+      }, 1000);
+    } else {
+      clearInterval(this.timerID);
+    }
   }
 
   render() {
     const icon = `fa-solid fa-${this.state.isOn ? 'pause' : 'play'}`;
     return (
       <div>
-        <div className="circle"><span className="number">{this.state.time}</span></div>
+        <div className="circle" onClick={this.reset}><span className="number">{this.state.time}</span></div>
         <div className="icon">
           <i className={icon} onClick={this.toggle}></i>
         </div>
