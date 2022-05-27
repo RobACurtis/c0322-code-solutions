c3ac8acb279ad79c2d87830fef4f74d319efacc5
Rob Curtis <Robanthonycurtis@gmail.com>
2022-05-27 18:54:15 +0000

    completed react-context and text file

--- react-context/client/app.jsx
+++ react-context/client/app.jsx
@@ -18,6 +18,7 @@ export default class App extends React.Component {
     };
     this.handleSignIn = this.handleSignIn.bind(this);
     this.handleSignOut = this.handleSignOut.bind(this);
+
   }
 
   componentDidMount() {
