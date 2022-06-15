d51f8a3e240e36eb05f70a424f3fa631d7edcfe3
Rob Curtis <Robanthonycurtis@gmail.com>
2022-06-15 02:16:32 +0000

    in progress

--- data-structures-stacks/count-values.js
+++ data-structures-stacks/count-values.js
@@ -1,11 +1,9 @@
 /* exported countValues */
 
 function countValues(stack) {
-  const stackArray = [];
   let index = 0;
-  while (stack.peek() !== undefined) {
-    stackArray[index] = stack.pop();
+  while (stack.pop() !== undefined) {
     index++;
   }
-  return (stackArray.length);
+  return index;
 }
