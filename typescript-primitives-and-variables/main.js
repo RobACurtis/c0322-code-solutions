bfb81ee14424ae52ef1325e87f02bd8a01f65060
Rob Curtis <Robanthonycurtis@gmail.com>
2024-02-13 17:31:36 -0800

    completed

---
+++ typescript-primitives-and-variables/main.js
@@ -0,0 +1,15 @@
+var fullName = 'DJDRob';
+var isCool = false;
+var totalPets = 0;
+console.log('fullName: ', fullName);
+console.log('isCool: ', isCool);
+console.log('totalPets: ', totalPets);
+console.log('The type of fullName is: ', typeof fullName);
+console.log('The type of isCool is: ', typeof isCool);
+console.log('The type of totalPets is: ', typeof totalPets);
+var unknown = 'unknown';
+console.log('unknown: ', unknown);
+console.log('The type of unknown is: ', typeof unknown);
+var never;
+console.log('never: ', never);
+console.log('The type of never is: ', typeof never);
